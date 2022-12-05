221ae580d06c6b7c1cf2cdcecf8d0b5bebc797d2
GuadalupeAlori <guadalupealori@yahoo.com.ar>
2022-12-05 08:11:58 -0300

    Cadena de texto en models

--- AppBiblioteca/models.py
+++ AppBiblioteca/models.py
@@ -9,17 +9,27 @@ class Libro(models.Model):
     tipo = models.CharField(max_length=50)
     codigo = models.IntegerField()
 
+    def __str__(self):
+        return self.titulo+" "+self.autor+" "+self.tipo+" "+str(self.codigo)
+
+
 class Afiliado(models.Model):
     
     nombre=models.CharField(max_length=50)
     apellido=models.CharField(max_length=50)
     email = models.EmailField()
     retiro = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nombre+" "+self.apellido+" "+self.email+" "+str(self.retiro)
    
 class Retiros(models.Model):
     
     codigoLibro =models.IntegerField()
     nombreAfiliado = models.CharField(max_length=50)
     fechaRetiro = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return str(self.codigoLibro)+" "+self.nombreAfiliado+" "+str(self.fechaRetiro)
 
     
